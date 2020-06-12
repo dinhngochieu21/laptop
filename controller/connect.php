0518814ed3086cbb76172f6e0be29f0d0ed75000
dinhngochieu21 <dinhngochieu21@gmail.com>
2020-06-12 22:22:31 +0700

    a

--- controller/connect.php
+++ controller/connect.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect('localhost','hieu21','hieuhieu21','my_csdl') or die('Unable To connect');
+$con = mysqli_connect('db4free.net:3306','hieu21','hieuhieu21','my_sql') or die('Unable To connect');
 ?>
